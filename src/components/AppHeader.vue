4928b33dc9136d257fcca1470ee3257030b27d9f
EvgenyFirst <>
2021-11-07 17:08:31 +0600

    Выполненыне задания в одном коммите 1 и 2

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navber-Light bg-dark">
+    <div class="container-fluid">
+      <a
+        href="#"
+        class="navbar-brand"
+        style="color: #ffffff; text-decoration: none"
+        >Marvel</a
+      >
+      <form action="/" class="d-flex">
+        <input
+          type="search"
+          class="form-control me-2"
+          placeholder="Поиск..."
+          aria-label="search"
+        />
+        <button type="submit" class="btn btn-outline-light">Все</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
